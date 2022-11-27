75a140adf21719cc58724d1c0beaf672851f59ab
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-11-27 20:16:08 +0100

    fix: 🐛 Fix update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -49,14 +49,14 @@ echo "::set-output name=NEW_PKGVER::$NEW_PKGVER"
 
 # Update checksums
 if [[ $INPUT_UPDPKGSUMS == true ]]; then
-    # echo "::group::Cleaning build directory"
-    # # Delete all files except PKGBUILD
-    # find . -maxdepth 1 -not \( -name '.' -or -name 'PKGBUILD' \) -exec rm -rvf {} +
-    # # Copy other files
-    # cp -fv "$WORKPATH"/!(PKGBUILD) ./
-    # # List existing files
-    # ls -a
-    # echo "::endgroup::"
+    echo "::group::Cleaning build directory"
+    # Delete all files except PKGBUILD
+    find . -maxdepth 1 -not \( -name '.' -or -name 'PKGBUILD' \) -exec rm -rvf {} +
+    # Copy other files
+    cp -fv "$WORKPATH"/!(PKGBUILD) ./
+    # List existing files
+    ls -a
+    echo "::endgroup::"
     echo "::group::Updating checksums on PKGBUILD"
     updpkgsums
     git diff PKGBUILD
